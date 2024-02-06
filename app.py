b6a80a9873eef33ada6b86f016e2c11419c0bb52
Victor Kim <vskimjr@gmail.com>
2024-02-06 18:06:41 -0500

    created Cupcake model, added first route to get all cupcakes

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
 """Flask app for Cupcakes"""
+
+import os
+from flask import Flask, request, jsonify
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    "DATABASE_URL", 'postgresql:///cupcakes')
+
+connect_db(app)
+
+
+@app.get("/api/cupcakes")
+def list_all_cupcakes():
+    """ Get data about all cupcakes Respond with JSON like: {cupcakes:
+    [{id, flavor, size, rating, image_url}, ...]}
+    """
+
+    cupcakes = Cupcake.query.all()
+    serialized = [c.serialize() for c in cupcakes]
+    return jsonify(cupcakes=serialized)
+
--- models.py
+++ models.py
@@ -1,1 +1,45 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+
+class Cupcake(db.Model):
+    __tablename__ = "cupcakes"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    flavor = db.Column(
+        db.String(50),
+        nullable=False
+    )
+
+    size = db.Column(
+        db.String(15),
+        nullable=False
+    )
+
+    rating = db.Column(
+        db.Integer,
+        nullable=False
+    )
+
+    image_url = db.Column(
+        db.String(500),
+        nullable=False,
+        default="https://tinyurl.com/demo-cupcake"
+    )
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
+
+
